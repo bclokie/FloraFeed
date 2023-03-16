0c4557fb45608889a7f7cd3020ce0c7a8d2c458b
Braden <braden.clokie@gmail.com>
2023-03-16 15:47:48 -0400

    Added catsRoutes

---
+++ server/routes/catsRoutes.js
@@ -0,0 +1,10 @@
+/////////// catsRoutes.js
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  const cats = ['Rosey', 'Puma', 'Mr Buttons', 'Aya'];
+  res.json(cats);
+});
+  
+
+module.exports = router;
