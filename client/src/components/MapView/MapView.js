c6c43f08bbb553010c043ee94b659c55c84b25b5
Braden <braden.clokie@gmail.com>
2023-03-21 00:22:41 -0400

    Added 📍 for Alex

--- client/src/components/MapView/MapView.js
+++ client/src/components/MapView/MapView.js
@@ -16,7 +16,8 @@ const coordinatesData = [
   { id: 1, position: { lat: 37.7749, lng: -122.4194 } },
   { id: 2, position: { lat: 40.7128, lng: -74.0060 } },
   { id: 3, position: { lat: 51.5074, lng: -0.1278 } },
-  { id: 4, position: { lat: 43.642567, lng: -79.387054 } }
+  { id: 4, position: { lat: 43.642567, lng: -79.387054 } },
+  { id: 5, position: { lat: -33.4337, lng: -70.6510 } }
 ]
 
 function MapView() {
