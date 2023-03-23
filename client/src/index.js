fa5802b9c20f06f59d2fb5d81d936781a9a9905f
Corey Silver <coreysilver@outlook.com>
2023-03-22 20:43:50 -0400

    Fixed issue with google maps giving an error after loading

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { LoadScript } from "@react-google-maps/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}></LoadScript>
     <App />
   </React.StrictMode>
 );
