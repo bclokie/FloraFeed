0b5719979ae1444d2e1c02326add40c2c5bbcabc
Braden <braden.clokie@gmail.com>
2023-03-23 01:27:14 -0400

    Updated readme and fixed dependency issue

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-#Hello!
+## Welcome to our Final Project
 
-Placeholder for the README file contents.
+### A collaborative project between:
 
-Collaborative project between @bclokie, @corey237, and @JimHwkins.
+# [Alejandro Ferru](https://github.com/JimHwkins/)
+
+# [Braden Clokie](https://github.com/bclokie/)
+
+# [Corey Silver](https://github.com/corey237/)  
+
+
+## What is this project?
+
+This project is a simple web application that allows users to... The application is built using the MERN stack (MongoDB, Express, React, Node.js) with image hosting done on Firebase. The application is currently live at: https://final-project-2.herokuapp.com/ (JUST KIDDING NOT YET)
+
+## How to run the application locally
+
+1. Clone the repository
+2. Run `npm install` in the root directory
+3. Run `npm start` in the root directory
+4. Navigate to `localhost:3000` in your browser
+
+## How to run the tests
+
+1. Run `npm run test` in the root directory
+
+## How to run the linter
+
+1. Run `npm run lint` in the root directory
+
+## How to run the linter and fix errors
+
+1. Run `npm run lint:fix` in the root directory
--- client/src/components/Submit/SubmitStyles.js
+++ client/src/components/Submit/SubmitStyles.js
@@ -1,5 +1,5 @@
-import { makeStyles } from '@mui/styles';
 import { createTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 const theme = createTheme();
 const colors = {
@@ -10,47 +10,53 @@ const colors = {
   lightGreen2: "#DAE1D8",
 };
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  input: {
-    backgroundColor: 'white',
-    border: '1px solid grey',
-    borderRadius: '4px',
-    padding: '8px 12px',
-    fontSize: '16px',
-    width: '100%',
-    boxSizing: 'border-box',
-    marginBottom: '16px',
-  },
-  textarea: {
-    backgroundColor: 'white',
-    border: '1px solid grey',
-    borderRadius: '4px',
-    padding: '8px 12px',
-    fontSize: '16px',
-    width: '100%',
-    boxSizing: 'border-box',
-    height: '100px',
-    marginBottom: '16px',
-  },
-  button: {
-    backgroundColor: colors.green1,
-    border: 'none',
-    borderRadius: '4px',
-    color: 'white',
-    padding: '12px 24px',
-    fontSize: '16px',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
-    '&:hover': {
-      backgroundColor: colors.green2,
-    },
-    display: 'block',
-    margin: '0 auto',
-  },
-}));
+const Input = styled('input')({
+  backgroundColor: colors.white,
+  border: '1px solid grey',
+  borderRadius: '4px',
+  padding: '8px 12px',
+  fontSize: '16px',
+  width: '100%',
+  boxSizing: 'border-box',
+  marginBottom: '16px',
+});
 
-export { theme, useStyles };
+const TextArea = styled('textarea')({
+  backgroundColor: colors.white,
+  border: '1px solid grey',
+  borderRadius: '4px',
+  padding: '8px 12px',
+  fontSize: '16px',
+  width: '100%',
+  boxSizing: 'border-box',
+  height: '100px',
+  marginBottom: '16px',
+});
+
+const Button = styled('button')({
+  backgroundColor: colors.green1,
+  border: 'none',
+  borderRadius: '4px',
+  color: 'white',
+  padding: '12px 24px',
+  fontSize: '16px',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s',
+  '&:hover': {
+    backgroundColor: colors.green2,
+  },
+  display: 'block',
+  margin: '0 auto',
+});
+
+function Form() {
+  return (
+    <div>
+      <Input type="text" />
+      <TextArea />
+      <Button>Submit</Button>
+    </div>
+  );
+}
+
+export { theme, Form };
