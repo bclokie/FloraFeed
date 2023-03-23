2ce2923bb288d8300350dcc32d12291119629359
Corey Silver <coreysilver@outlook.com>
2023-03-23 16:48:52 -0400

    Refactored grid view to use google firebase instead of mongodb

--- client/src/components/GridView/GridView.js
+++ client/src/components/GridView/GridView.js
@@ -1,52 +1,47 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import PlantDetails from "../PlantDetails/PlantDetails";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
 
-const containerStyle = {
-  width: "100%",
-  margin: "auto",
-  height: "100%",
-};
-
-
-const postData = [];
-const getPosts = function() {
-  axios.get('http://localhost:8080/posts')
-  .then((posts) => {
-    posts.data.map((data, index) => {
-      const date = new Date(data.created_at);
-      const formattedDate = date.toLocaleDateString();
-      const formattedTime = date.toLocaleTimeString();
-      postData.push({
-        id: index + 1,
-        user: {
-          userName: 'Brandy',
-          userAvatar: 'https://source.unsplash.com/random/100x100'
-        },
-        plant: {
-          commonName: data.title,
-          scientificName: data.plantName,
-          description: data.description,
-          imageUrl: data.image,
-          timePosted: `${formattedDate} ${formattedTime}`
-        }
-      })
-    })
-  })
-}
-getPosts()
-
 const GridView = () => {
-
-  console.log('posts data is: ', postData)
+  let [postData, setPostData] = useState([]);
+  useEffect(() => {
+    const temp = [];
+    axios
+      .get(
+        "https://firestore.googleapis.com/v1/projects/final-project-lhl-a053a/databases/(default)/documents/posts"
+      )
+      .then((posts) => {
+        posts.data.documents.map((data, index) => {
+          const date = new Date(data.createTime);
+          const formattedDate = date.toLocaleDateString();
+          const formattedTime = date.toLocaleTimeString();
+          temp.push({
+            id: index + 1,
+            user: {
+              userName: "Brandy",
+              userAvatar: "https://source.unsplash.com/random/100x100",
+            },
+            plant: {
+              commonName: data.fields.title.stringValue,
+              scientificName: data.fields.plantName.stringValue,
+              description: data.fields.description.stringValue,
+              imageUrl: data.fields.image.stringValue,
+              timePosted: `${formattedDate} ${formattedTime}`,
+            },
+          });
+        });
+        setPostData(temp);
+      });
+  }, []);
   return (
     <Grid container spacing={1} sx={{ width: "100%", height: "100%" }}>
-      {postData.map((data) => (
-        <Grid item xs={12} sm={12} md={6} lg={4} key={data.id}>
-          <PlantDetails user={data.user} plant={data.plant} />
-        </Grid>
-      ))}
+      {postData.length > 0 &&
+        postData.map((data) => (
+          <Grid item xs={12} sm={12} md={6} lg={4} key={data.id}>
+            <PlantDetails user={data.user} plant={data.plant} />
+          </Grid>
+        ))}
     </Grid>
   );
 };
